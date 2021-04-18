65c1de107ddd8b0d3e3678f988e897545823b65c
alexdewaal66 <alexdewaal@gmail.com>
2021-04-18 17:10:47 +0200

    removed implementation

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ module.exports = {
     challenge: challenge
 }
 
-function challenge(x) {
-    const {garbled} = x;
-    let result = '';
-    for (let i = 1; i < garbled.length; i += 3) {
-        result += garbled.charAt(i);
-    }
-    return result;
+function challenge() {
+    // Have fun!
 }
