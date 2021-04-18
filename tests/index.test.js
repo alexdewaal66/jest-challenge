e8c239acb6b6c7d4b231cee3e6f225e96ceb0a34
alexdewaal66 <alexdewaal@gmail.com>
2021-04-18 16:56:54 +0200

    cleaned up formatting

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-const { challenge } = require('../index');
+const {challenge} = require('../index');
 
 test(`challenge(x) will extract every 2nd char out of 3 from 'x.garbled' and concatenate them.`, () => {
     // Arrange
@@ -7,8 +7,10 @@ test(`challenge(x) will extract every 2nd char out of 3 from 'x.garbled' and con
         garbled: 'WO.ep.rd.krf aaic nht teu iivnto vlgobneraralic nhggto.!N',
         useless: null
     }
+
     // Act
     const result = challenge(testObject);
+
     // Assert
     expect(typeof result).toBe('string');
     expect(result).toEqual('Opdracht volbracht!');
